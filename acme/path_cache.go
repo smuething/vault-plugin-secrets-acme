b239e6f29101f17612b894fdcfe4a950c5a44e45
Steffen Müthing <steffen.muething@iwr.uni-heidelberg.de>
2024-10-28 14:08:22 +0100

    Add a list operation for the cache

--- acme/path_cache.go
+++ acme/path_cache.go
@@ -7,20 +7,25 @@ import (
 	"github.com/hashicorp/vault/sdk/logical"
 )
 
-func pathCache(b *backend) *framework.Path {
-	return &framework.Path{
-		Pattern: "cache",
-		Fields: map[string]*framework.FieldSchema{
-			"cached_certs": {
-				Type: framework.TypeInt,
+func pathCache(b *backend) []*framework.Path {
+	return []*framework.Path{
+		{
+			Pattern: "cache",
+			Fields: map[string]*framework.FieldSchema{
+				"cached_certs": {
+					Type: framework.TypeInt,
+				},
 			},
-		},
-		Operations: map[logical.Operation]framework.OperationHandler{
-			logical.ReadOperation: &framework.PathOperation{
-				Callback: b.cacheRead,
-			},
-			logical.DeleteOperation: &framework.PathOperation{
-				Callback: b.cacheClear,
+			Operations: map[logical.Operation]framework.OperationHandler{
+				logical.ReadOperation: &framework.PathOperation{
+					Callback: b.cacheRead,
+				},
+				logical.DeleteOperation: &framework.PathOperation{
+					Callback: b.cacheClear,
+				},
+				logical.ListOperation: &framework.PathOperation{
+					Callback: b.cacheList,
+				},
 			},
 		},
 	}
@@ -49,4 +54,37 @@ func (b *backend) cacheClear(ctx context.Context, req *logical.Request, _ *frame
 		return logical.ErrorResponse("failed to clear cache"), err
 	}
 	return nil, nil
+}
+
+func (b *backend) cacheList(ctx context.Context, req *logical.Request, _ *framework.FieldData) (*logical.Response, error) {
+	b.cache.Lock()
+	defer b.cache.Unlock()
+
+	keys, err := b.cache.List(ctx, req.Storage)
+	if err != nil {
+		return nil, err
+	}
+
+	b.Logger().Debug("Listing cache keys", "keys", keys)
+
+	keyInfo := make(map[string]interface{})
+	for _, key := range keys {
+		ce, err := b.cache.Read(ctx, req.Storage, key)
+		if err != nil {
+			return nil, err
+		}
+		if ce == nil {
+			continue
+		}
+		keyInfo[key] = map[string]interface{}{
+			"account":        ce.Account,
+			"role":           ce.Role,
+			"primary_domain": ce.Domain,
+			"domains":        ce.Domains,
+			"leases":         ce.Leases,
+			"certificates":   len(ce.Certificates),
+		}
+	}
+
+	return logical.ListResponseWithInfo(keys, keyInfo), nil
 }
